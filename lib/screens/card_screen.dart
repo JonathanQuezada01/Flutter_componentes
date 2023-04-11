cf2ce94aef49dcfc7fbc41e7653681c5dc2cec6d
Jonathan Isaac Quezada Anguiano <jonathan.quezada@beesoftware.mx>
2023-04-10 19:06:46 -0600

    finalizacion de las cards

--- lib/screens/card_screen.dart
+++ lib/screens/card_screen.dart
@@ -15,7 +15,31 @@ class CardScreen extends StatelessWidget {
         ///
         body: ListView(
           padding: const EdgeInsets.symmetric(horizontal: 0, vertical: 20),
-          children: const [CustomCardType1(), CustomCardType2()],
+          children: const [
+            CustomCardType1(),
+            SizedBox(height: 5), //espacio bajo la tarjeta
+            CustomCardType2(
+              imageName: 'Que buen culo ',
+              imageUrl:
+                  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTEh5nBynFl-UaruEtNgMXMZMlJPion1uTPEA&usqp=CAU',
+            ),
+            SizedBox(height: 5),
+            CustomCardType2(
+              imageUrl:
+                  'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/513vvwBzJfS.jpg',
+            ),
+            SizedBox(height: 5),
+            CustomCardType2(
+              imageUrl:
+                  'https://www.rollingstone.com/wp-content/uploads/2019/07/bathwatergamer.jpg',
+            ),
+            SizedBox(height: 5),
+            CustomCardType2(
+              imageUrl:
+                  'https://i.kym-cdn.com/entries/icons/original/000/040/215/belledelphinereturn.jpg',
+            ),
+            SizedBox(height: 30),
+          ],
         ));
   }
 }
--- lib/widgets/custom_card_type_2.dart
+++ lib/widgets/custom_card_type_2.dart
@@ -2,36 +2,37 @@ import 'package:componentes/themes/app_themes.dart';
 import 'package:flutter/material.dart';
 
 class CustomCardType2 extends StatelessWidget {
-  const CustomCardType2({super.key});
-
+  const CustomCardType2({super.key, required this.imageUrl, this.imageName});
+  final String imageUrl;
+  final String? imageName;
   @override
   Widget build(BuildContext context) {
     return Card(
       clipBehavior: Clip.antiAliasWithSaveLayer,
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(50)),
-      elevation: 10,
-      shadowColor: AppTheme.primaryColor.withOpacity(.5),
+      elevation: 0,
+      shadowColor: AppTheme.primaryColor.withOpacity(.3),
       child: Column(
         children: [
           const SizedBox(
-            height: 20,
             width: 20,
           ),
-          const FadeInImage(
-            image: NetworkImage(
-                'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTEh5nBynFl-UaruEtNgMXMZMlJPion1uTPEA&usqp=CAU'),
-            placeholder: AssetImage('assets/download.png'),
+          FadeInImage(
+            image: NetworkImage(imageUrl),
+            placeholder: const AssetImage('assets/download.png'),
             width: double.infinity,
             height: 230,
             fit: BoxFit.cover,
-            fadeInDuration: Duration(
+            fadeInDuration: const Duration(
                 seconds: 1), //Tiempo que tarda la imagen en hacerse visible
           ),
-          Container(
-            alignment: AlignmentDirectional.bottomEnd,
-            padding: const EdgeInsets.only(right: 20, top: 10, bottom: 10),
-            child: const Text('que buen culo'),
-          ),
+          if (imageName != null)
+            Container(
+              alignment: AlignmentDirectional.bottomEnd,
+              padding: const EdgeInsets.only(right: 20, bottom: 10),
+              color: Colors.amber,
+              child: const Text('Que tipa tan rara'),
+            ),
         ],
       ),
     );
