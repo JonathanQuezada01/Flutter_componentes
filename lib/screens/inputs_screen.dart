2c1972cffcc74d42c15ac27bdbed2586d72883d0
Jonathan Isaac Quezada Anguiano <jonathan.quezada@beesoftware.mx>
2023-05-22 20:37:45 -0600

    Creacion de input personalizado

--- lib/screens/inputs_screen.dart
+++ lib/screens/inputs_screen.dart
@@ -7,10 +7,44 @@ class InputsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Inputs'),
+        title: const Text('Inputs y forms'),
       ),
-      body: const Center(
-        child: Text('InputsScreen'),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+          child: Column(
+            children: [
+              TextFormField(
+                autofocus: true,
+                initialValue: '', //Valor inicial del campo
+                autocorrect: true,
+                textCapitalization: TextCapitalization.words,
+                validator: (value) {
+                  if (value == null) {
+                    return 'Este campo es requerido';
+                  }
+                  //  else if (value.isNotEmpty) {
+                  //   return icon: Icon(Icons.check_box_icon);
+                  // }
+                  return value.isEmpty ? 'Debes de poner algo' : null;
+                },
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                decoration: const InputDecoration(
+                    helperText: 'Nombre completo',
+                    hintText: 'Nombre del usuario',
+                    labelText: 'Coloca tu nombre aquí',
+                    suffixIcon: Icon(Icons.person_pin_outlined),
+                    focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.purple)),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.only(
+                            bottomLeft: Radius.circular(10),
+                            topRight: Radius.circular(10))),
+                    icon: Icon(Icons.check_circle_outline_rounded)),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
